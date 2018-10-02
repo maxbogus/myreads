3c2ccc31214bbd58e0b513052ad89eb2cd0a5f45
Max Boguslavskiy <max.bogus@gmail.com>
2018-10-03 01:09:48 +0300

    - added routing and links to project.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
+import {Route, Link} from 'react-router-dom'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -11,15 +12,15 @@ class BooksApp extends React.Component {
          * pages, as well as provide a good URL they can bookmark and share.
          */
         showSearchPage: false
-    }
+    };
 
     render() {
         return (
             <div className="app">
-                {this.state.showSearchPage ? (
+                <Route exact path='/search' render={() => (
                     <div className="search-books">
                         <div className="search-books-bar">
-                            <a className="close-search" onClick={() => this.setState({showSearchPage: false})}>Close</a>
+                            <Link className="close-search" to='/'>Close</Link>
                             <div className="search-books-input-wrapper">
                                 {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -36,7 +37,8 @@ class BooksApp extends React.Component {
                             <ol className="books-grid"></ol>
                         </div>
                     </div>
-                ) : (
+                )}/>
+                <Route exact path='/' render={() => (
                     <div className="list-books">
                         <div className="list-books-title">
                             <h1>MyReads</h1>
@@ -229,10 +231,10 @@ class BooksApp extends React.Component {
                             </div>
                         </div>
                         <div className="open-search">
-                            <a onClick={() => this.setState({showSearchPage: true})}>Add a book</a>
+                            <Link to='/search'>Add a book</Link>
                         </div>
                     </div>
-                )}
+                )}/>
             </div>
         )
     }
